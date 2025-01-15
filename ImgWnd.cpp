7b12775895506037812ebd43ee3dd25a4c3cb4cc
hspn <hspn94@gmail.com>
2025-01-15 13:09:01 +0900

    fix: 픽셀 읽는순서 조정
    세로 역순, 가로 순방향, 색상 역순으로 입력하니 정상적으로 나옴

--- ImgWnd.cpp
+++ ImgWnd.cpp
@@ -74,8 +74,9 @@ LRESULT CImgWnd::_ReadImage(FILE* fp) noexcept
 		jpeg_destroy_decompress(&cinfo);
 		return -1;
 	}
-	
-	auto imgData = (JSAMPLE *)malloc(cinfo.image_width * cinfo.image_height * 3);// cinfo.jpeg_color_space);
+
+	auto s_imgData = cinfo.output_width * cinfo.output_components;
+	auto imgData = (JSAMPLE *)malloc(s_imgData);// cinfo.jpeg_color_space);
 	if (imgData == nullptr) {
 		MessageBox(_T("메모리 부족"), _T("Notification"), MB_OK);
 		jpeg_destroy_decompress(&cinfo);
@@ -109,16 +110,16 @@ LRESULT CImgWnd::_ReadImage(FILE* fp) noexcept
 	auto hdc = GetDC();
 	auto hMemDC = CreateCompatibleDC(hdc); //직접채우지않고 다른 DC에 적어놓고 교체
 	hBitmap = CreateDIBSection(hdc, &bitmapInfo, DIB_RGB_COLORS, &ppvBits, NULL, 0);
-	
+
 	auto cursor = reinterpret_cast<BYTE*>(ppvBits);
-	cursor += cinfo.output_height * cinfo.output_width * cinfo.output_components - 1; //역순입력
-	auto s_imgData = cinfo.image_width * cinfo.output_components;
+	cursor += cinfo.output_height * s_imgData - 1; //역순입력을 위해, 버퍼 끝에서 시작
 	
 	while (cinfo.output_scanline < cinfo.output_height) {
 		auto ret = jpeg_read_scanlines(&cinfo, &imgData, 1);
-		//memcpy(cursor, imgData, s_imgData);
-		for (auto i = 0; i < s_imgData; i++)
-			cursor[-i] = imgData[i];
+		for (int i = 0; i < s_imgData; i+=cinfo.output_components) //세로는 역순입력, 가로는 같은순서입력, 픽셀 내 색상은 역순입력
+			for (int j = 0; j < cinfo.output_components; j++)
+				cursor[i+(cinfo.output_components - j) - (int)s_imgData] = imgData[i + j];
+
 		cursor -= s_imgData;
 	}
 	
